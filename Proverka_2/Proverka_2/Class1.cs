8f567c1af94265a4cee38506c22a280b3fabc415
Zelenin-4-22 <158761884+Zelenin-4-22@users.noreply.github.com>
2024-09-26 01:51:12 +0300

    Add files via upload

---
+++ Proverka_2/Proverka_2/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+internal class Labotary2
+{
+    double one;
+    double two;
+
+    public void plus(double one, double two)
+    {
+        Console.WriteLine(one + two);
+    }
+    public void minus(double one, double two)
+    {
+        Console.WriteLine(one - two);
+    }
+    public void umnoshenie(double one, double two)
+    {
+        Console.WriteLine(one * two);
+    }
+    public void delenie(double one, double two)
+    {
+        if (two == 0)
+        {
+            Console.WriteLine("Некорректно");
+        }
+        else
+        {
+            Console.WriteLine(one / two);
+        }
+
+    }
+    public void sqrt(double one, double two)
+    {
+        Console.WriteLine(Math.Sqrt(one));
+        Console.WriteLine(Math.Sqrt(two));
+    }
+    public void stepen(double one, double two)
+    {
+        Console.WriteLine(Math.Pow(one, two));
+    }
+    public void okryglenie(double one, double two)
+    {
+        Console.WriteLine(Math.Round(one));
+    }
+
+    public void arksin(double one, double two)
+    {
+        Console.WriteLine(Math.Atan(one));
+    }
+    public void tan(double one, double two)
+    {
+        Console.WriteLine((double)Math.Tan(one));
+    }
+    public void tanh(double one, double two)
+    {
+        Console.WriteLine(Math.Tanh(one));
+    }
+    public void perimetr(double one, double two)
+    {
+        Console.WriteLine((one + two));
+    }
+    public void ploshadpr(double one, double two)
+    {
+        Console.WriteLine(one * two);
+    }
+    public void g(double one, double two)
+    {
+        Console.WriteLine(one * two);
+    }
+    public void plcube(double one)
+    {
+        Console.WriteLine(Math.Pow(one, 6));
+    }
+}
+
+    
---
+++ Proverka_2/Proverka_2/Program.cs
@@ -0,0 +1,71 @@
+﻿using ConsoleApp1;
+
+start: 
+
+Labotary2 lb2 = new Labotary2();
+
+string per;
+
+double x;
+double y;
+
+Console.WriteLine("Введите первое число");
+try { x = Convert.ToDouble(Console.ReadLine());}
+catch (Exception ex)
+{
+    x = 0;
+}
+
+Console.WriteLine("Введите операцию или функцию");
+per = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+try
+{
+    y = Convert.ToDouble(Console.ReadLine());
+}
+catch (Exception ex)
+{
+    y = 0;
+}
+
+switch(per)
+{
+    case "+":
+        lb2.plus(x, y);
+        goto start;
+    case "-":
+        lb2.minus(x, y);
+        goto start;
+    case "*":
+        lb2.umnoshenie(x, y);
+        goto start;
+    case "/":
+        lb2.delenie(x, y);
+        goto start;
+    case "степень":
+        lb2.stepen(x, y);
+        goto start;
+    case "sqrt":
+        lb2.sqrt(x, y);
+        goto start;
+    case "округление":
+        lb2.okryglenie(x, y);
+        goto start;
+    case "арксинус":
+        lb2.arksin(x, y);
+        goto start;
+    case "площадь прямоугольника":
+        lb2.ploshadpr(x, y);
+        goto start;
+    case "тангенс":
+        lb2.tan(x, y);
+        goto start;
+    case "гиперболический тангенс":
+        lb2.tanh(x, y);
+        goto start;
+    case "периметр прямоугольника":
+    default:
+        Console.WriteLine("Вы не ввели действие");
+        break;
+}
